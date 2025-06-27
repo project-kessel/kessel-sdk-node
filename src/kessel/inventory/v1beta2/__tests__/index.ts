81e3970889bf555631b4ed6316f0016eb61fd96c
Josejulio Martínez <jmartine@redhat.com>
2025-06-27 09:54:14 -0600

    - Ignore all inside __tests__ files - Test certs from strings

--- src/kessel/inventory/v1beta2/__tests__/index.ts
+++ src/kessel/inventory/v1beta2/__tests__/index.ts
@@ -61,6 +61,68 @@ describe("ClientBuilder", () => {
       expect(builder.credentials).toEqual(ChannelCredentials.createSsl());
     });
 
+    it("fromConfig allows to specify certificates", () => {
+      const certs = {
+        rootCerts:
+          "-----BEGIN CERTIFICATE-----\n" +
+          "MIIB5zCCAZmgAwIBAgIUQ4vriAZHUYcObyptYO6HGvCD6pYwBQYDK2VwMGkxCzAJ\n" +
+          "BgNVBAYTAkNBMRAwDgYDVQQIDAdBbGJlcnRhMREwDwYDVQQHDAhFZG1vbnRvbjEX\n" +
+          "MBUGA1UECgwOS2Vzc2VsIFRlc3QgQ0ExHDAaBgNVBAMME0tlc3NlbCBUZXN0IFJv\n" +
+          "b3QgQ0EwHhcNMjUwNjI3MTU0MjIwWhcNMzUwNjI1MTU0MjIwWjBpMQswCQYDVQQG\n" +
+          "EwJDQTEQMA4GA1UECAwHQWxiZXJ0YTERMA8GA1UEBwwIRWRtb250b24xFzAVBgNV\n" +
+          "BAoMDktlc3NlbCBUZXN0IENBMRwwGgYDVQQDDBNLZXNzZWwgVGVzdCBSb290IENB\n" +
+          "MCowBQYDK2VwAyEAcR9S57YlW0lcr+NTg+aZiEGfcwET9/p6tr1Ud7I4q/KjUzBR\n" +
+          "MB0GA1UdDgQWBBSz4Jqsyo2RvWuthYtSJkR+PCN25jAfBgNVHSMEGDAWgBSz4Jqs\n" +
+          "yo2RvWuthYtSJkR+PCN25jAPBgNVHRMBAf8EBTADAQH/MAUGAytlcANBAMjIzU22\n" +
+          "x1QBGHx2/ZUZegM6eZtsdT6DNv93NR9YfakSwqvSx3jl9UYVCG4bddmcIo8rqXwE\n" +
+          "rIeRqrbQC9iB+AU=\n" +
+          "-----END CERTIFICATE-----\n",
+        privateCerts:
+          "-----BEGIN PRIVATE KEY-----\n" +
+          "MC4CAQAwBQYDK2VwBCIEIN0MUcoTfYW0lTDOxmentzcPf8VhpAW+Xyifip/rKaKU\n" +
+          "-----END PRIVATE KEY-----",
+        certChain:
+          "-----BEGIN CERTIFICATE-----\n" +
+          "MIICBDCCAbagAwIBAgIUJRI7lf4qvyni0ebFuQv54gwbhKMwBQYDK2VwMGkxCzAJ\n" +
+          "BgNVBAYTAkNBMRAwDgYDVQQIDAdBbGJlcnRhMREwDwYDVQQHDAhFZG1vbnRvbjEX\n" +
+          "MBUGA1UECgwOS2Vzc2VsIFRlc3QgQ0ExHDAaBgNVBAMME0tlc3NlbCBUZXN0IFJv\n" +
+          "b3QgQ0EwHhcNMjUwNjI3MTU0MjU3WhcNMjYwNjI3MTU0MjU3WjBjMQswCQYDVQQG\n" +
+          "EwJDQTEQMA4GA1UECAwHQWxiZXJ0YTERMA8GA1UEBwwIRWRtb250b24xGzAZBgNV\n" +
+          "BAoMEktlc3NlbCBUZXN0IFNlcnZlcjESMBAGA1UEAwwJbG9jYWxob3N0MCowBQYD\n" +
+          "K2VwAyEAeGTFPQ0wA+Xw/3GstaL1eWXbOWWVDXcUje8EvuzUsZqjdjB0MB8GA1Ud\n" +
+          "IwQYMBaAFLPgmqzKjZG9a62Fi1ImRH48I3bmMAkGA1UdEwQCMAAwCwYDVR0PBAQD\n" +
+          "AgTwMBoGA1UdEQQTMBGCCWxvY2FsaG9zdIcEfwAAATAdBgNVHQ4EFgQUwsUD9F4j\n" +
+          "Mfa0NyKnOWFJKN/dMLQwBQYDK2VwA0EAPZ4++vH2/tyHFsbN7jezV+VHxzxJrjdS\n" +
+          "J2nPCvlvMEiVHG7d3O4mO3L6fHokMRm6ASjMcdJ/vZlGE/Uk3RnFCg==\n" +
+          "-----END CERTIFICATE-----\n" +
+          "-----BEGIN CERTIFICATE-----\n" +
+          "MIIB5zCCAZmgAwIBAgIUQ4vriAZHUYcObyptYO6HGvCD6pYwBQYDK2VwMGkxCzAJ\n" +
+          "BgNVBAYTAkNBMRAwDgYDVQQIDAdBbGJlcnRhMREwDwYDVQQHDAhFZG1vbnRvbjEX\n" +
+          "MBUGA1UECgwOS2Vzc2VsIFRlc3QgQ0ExHDAaBgNVBAMME0tlc3NlbCBUZXN0IFJv\n" +
+          "b3QgQ0EwHhcNMjUwNjI3MTU0MjIwWhcNMzUwNjI1MTU0MjIwWjBpMQswCQYDVQQG\n" +
+          "EwJDQTEQMA4GA1UECAwHQWxiZXJ0YTERMA8GA1UEBwwIRWRtb250b24xFzAVBgNV\n" +
+          "BAoMDktlc3NlbCBUZXN0IENBMRwwGgYDVQQDDBNLZXNzZWwgVGVzdCBSb290IENB\n" +
+          "MCowBQYDK2VwAyEAcR9S57YlW0lcr+NTg+aZiEGfcwET9/p6tr1Ud7I4q/KjUzBR\n" +
+          "MB0GA1UdDgQWBBSz4Jqsyo2RvWuthYtSJkR+PCN25jAfBgNVHSMEGDAWgBSz4Jqs\n" +
+          "yo2RvWuthYtSJkR+PCN25jAPBgNVHRMBAf8EBTADAQH/MAUGAytlcANBAMjIzU22\n" +
+          "x1QBGHx2/ZUZegM6eZtsdT6DNv93NR9YfakSwqvSx3jl9UYVCG4bddmcIo8rqXwE\n" +
+          "rIeRqrbQC9iB+AU=\n" +
+          "-----END CERTIFICATE-----\n",
+      };
+
+      const builder = ClientBuilder.builderFromConfig({
+        target: "localhost:1337",
+        credentials: {
+          type: "secure",
+          rootCerts: certs.rootCerts,
+          privateCerts: certs.privateCerts,
+          certChain: certs.certChain,
+        },
+      });
+
+      expect(() => builder.build()).not.toThrow();
+    });
+
     it("Uses default values", () => {
       const builder = ClientBuilder.builderFromConfig({
         target: "jbond:007",
